1a1e0ce5ea2af2682579c2ffdbb6ecfbc3bd6c41
jonathan_2428240080-star <jojo.nathan08112@gmail.com>
2026-03-11 14:13:36 +0700

    Create script.js

---
+++ pertemuan8/script.js
@@ -0,0 +1,20 @@
+const products = [
+    {
+        id: 1,
+        nama: "Sofa Minimalis Biru",
+        harga: 3200000,
+        diskon: 4500000,
+        kategori: "Ruang Tamu",
+        stok: 5,
+        gambar: "https://picsum.photos/id/1068/400/400&quot"
+    },
+    { id: 2, nama: "Meja Makan Jati", harga: 5800000, diskon: 0, kategori: "Dapur", stok: 2, gambar: "https://picsum.photos/id/445/400/400&quot;" },
+    { id: 3, nama: "Rak Buku Oak", harga: 1500000, diskon: 0, kategori: "Ruang Kerja", stok: 0, gambar: "https://picsum.photos/id/367/400/400&quot;" },
+    { id: 4, nama: "Meja Kerja Ergonomis", harga: 2100000, diskon: 0, kategori: "Ruang Kerja", stok: 8, gambar: "https://picsum.photos/id/20/400/400&quot" },
+    { id: 5, nama: "Lampu Tidur Artistik", harga: 4500000, diskon: 0, kategori: "Kamar Tidur", stok: 12, gambar: "https://picsum.photos/id/48/400/400&quot;"},
+    { id: 6, nama: "Kursi Santai Teras", harga: 850000, diskon: 1200000, kategori: "Outdoor", stok: 4, gambar: "https://picsum.photos/id/10/400/400&quot;" },
+    { id: 7, nama: "Lemari Pakaian Slide", harga: 4200000, diskon: 0, kategori: "Kamar Tidur", stok: 3, gambar: "https://picsum.photos/id/449/400/400&quot;" },
+    { id: 8, nama: "Meja Rias Modern", harga: 2750000, diskon: 3000000, kategori: "Kamar Tidur", stok: 0, gambar: "https://picsum.photos/id/635/400/400&quot;" },
+    { id: 9, nama: "Kursi Makan Velvet", harga: 950000, diskon: 0, kategori: "Dapur", stok: 10, gambar: "https://picsum.photos/id/192/400/400&quot;" },
+    { id: 10, nama: "Karpet Bulu Mewah", harga: 1200000, diskon: 1500000, kategori: "Ruang Tamu", stok: 6, gambar: "https://picsum.photos/id/201/400/400&quot; "},
+];
