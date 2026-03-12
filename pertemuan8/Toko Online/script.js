7b0eb13716e01366549b7438ecb1200e59d8a484
Jonathan081105 <jojo.nathan08112@gmail.com>
2026-03-12 15:01:13 +0700

    Membuat Toko Online masih proses

---
+++ pertemuan8/Toko Online/script.js
@@ -0,0 +1,12 @@
+<script src="js/script.js"></script>
+
+function tambahKeranjang(nama, harga){
+
+let produk = {
+nama: nama,
+harga: harga
+}
+
+console.log("Produk ditambahkan:", produk)
+
+}
--- pertemuan8/script.js
+++ pertemuan8/script.js
@@ -37,3 +37,5 @@ function renderProducts() {
 
         }
     })}
+    function renderProducts(){
+        products.forEach {product
